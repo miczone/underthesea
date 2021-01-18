44f626eb3a64b20443a1d9ff367588761267b1e4
anhv <anhv.ict91@gmail.com>
2021-01-18 13:44:06 +0700

    update

--- tests/lab/dp_train.py
+++ tests/lab/dp_train.py
@@ -1,12 +1,12 @@
 from os.path import join
 
-from underthesea.datasets.vlsp2020_dp import VLSP2020_DP
+from underthesea.datasets.vlsp2020_dp import VLSP2020_DP_SAMPLE
 from underthesea.file_utils import MODELS_FOLDER
 from underthesea.models.dependency_parser import DependencyParser
 from underthesea.modules.embeddings import FieldEmbeddings, CharacterEmbeddings
 from underthesea.trainers.dependency_parser_trainer import DependencyParserTrainer
 
-corpus = VLSP2020_DP()
+corpus = VLSP2020_DP_SAMPLE()
 
 embeddings = [
     FieldEmbeddings(),
@@ -15,7 +15,7 @@ embeddings = [
 parser = DependencyParser(embeddings=embeddings, init_pre_train=True)
 trainer: DependencyParserTrainer = DependencyParserTrainer(parser, corpus)
 trainer.train(
-    base_path=join(MODELS_FOLDER, 'parsers', 'vi-dp-v1a0'),
-    max_epochs=1000,
+    base_path=join(MODELS_FOLDER, 'parsers', 'vi-dp-sample'),
+    max_epochs=10,
     mu=.9  # optimizer parameters
 )
